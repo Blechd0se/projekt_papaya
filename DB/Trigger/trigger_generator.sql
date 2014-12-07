57e916eba29726a4a1f403026ff01573556f22e7
Blechd0se <alex.christ@hotmail.de>
2014-12-07 01:22:23 +0100

    db: trigger: Generatoranpassungen
     * MySQL erfordet dies für einen Default
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- DB/Trigger/trigger_generator.sql
+++ DB/Trigger/trigger_generator.sql
@@ -1,24 +1,21 @@
 /**
  * Autor: Alexander Christ
- * Datum: 03.12.2014
+ * Datum: 07.12.2014
  * Thema: Generiert Trigger für Tabellen mit _erstellt_von Spalte
  */
  
- SELECT CONCAT(CONCAT(CONCAT(
+ SELECT CONCAT(CONCAT(
             CONCAT(
                 CONCAT(
                     CONCAT(
                         CONCAT(CONCAT(CONCAT('DROP TRIGGER trg_', CONCAT(table_name, '_insert;\n\n')), 'CREATE TRIGGER trg_'), table_name), 
-                    '_insert\n    AFTER INSERT ON '), 
+                    '_insert\n    BEFORE INSERT ON '), 
                 table_name), 
             '\n    FOR EACH ROW\n    BEGIN\n\n'),
-        '        UPDATE '),
-        CONCAT(
-            CONCAT(table_name, ' \n           SET '), 
-                CONCAT(column_name, ' = CURRENT_USER()\n')
+        CONCAT('       SET NEW.', 
+                CONCAT(column_name, ' = CURRENT_USER();\n')
               )
-        ),
-        CONCAT('         WHERE '), CONCAT(SUBSTRING(column_name, 1, 3), CONCAT('nr = NEW.', CONCAT(SUBSTRING(column_name, 1, 3), 'nr; \n\n    END;\n'))))
+        ), '\n    END;\n'), table_name
    FROM information_schema.COLUMNS
   WHERE column_name LIKE '%_erstellt_von'
     AND TABLE_NAME NOT LIKE 'v0_%'
