5aee0799afadd2ea09f2ff18d157cd05d372bb59
Blechd0se <alex.christ@hotmail.de>
2014-12-13 23:40:54 +0100

    db: views: v_mensch_firma_daten hinzugefügt
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

---
+++ DB/Views/v_mensch_firma_daten.sql
@@ -0,0 +1,24 @@
+/**
+ * Autor: Alexander Christ
+ * Datum: 13.12.2014
+ * Thema: Liefert alle bekannten Daten fuer die Firma eines Menschen
+ */
+CREATE OR REPLACE VIEW v_mensch_firma_daten
+ AS  
+SELECT me_nr,
+       mf_abteilung,
+       fi_bezeichnung,
+       ko_telefon,
+       ko_mobil,
+       ko_email,
+       ko_webseite,
+       ko_fax,
+       ad_strasse_nr,
+       ad_plz,
+       ad_ort
+  FROM v0_me_mensch
+       JOIN v0_mensch_firma_zuord    ON (mf_me_nr = mf_nr)
+       JOIN v0_fi_firma              ON (fi_mf_nr = fi_nr)
+       LEFT OUTER JOIN v0_ko_kontakt ON (ko_fi_nr = ko_nr)
+       LEFT OUTER JOIN v0_ad_adresse ON (ad_fi_nr = fi_nr)
+   ;
