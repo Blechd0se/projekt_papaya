6f6164c6285a5bad258887ecd99c60313bcf0af2
Blechd0se <alex.christ@hotmail.de>
2014-12-14 14:39:40 +0100

    db: functions: setze_status_mensch() hinzugefügt
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

---
+++ DB/Functions/setze_status_mensch.sql
@@ -0,0 +1,39 @@
+DROP FUNCTION setze_status_mensch;
+
+/**
+ * Autor: Alexander Christ
+ * Datum: 13.12.2014
+ * Thema: Liefert bei Erfolg 1 zurück, ansonsten 0
+ * 
+ * iME_NR, Mensch_Nr, die einen neuen Status erhalten soll
+ * iST_NR, Status_Nr, die Status Nr, die gesetzt werden soll
+ * iUS_NR, User_nr, der den Status geändert hat
+ * iSL_KOMMENTAR, Optionales Kommentar
+ */
+CREATE FUNCTION setze_status_mensch(
+    iME_NR INT,
+    iST_NR INT,
+    iUS_NR INT,
+    iSL_KOMMENTAR VARCHAR(4000)
+  ) RETURNS INT
+    DETERMINISTIC
+BEGIN
+
+    DECLARE v_aktuelle_st_nr INT DEFAULT NULL;
+
+    SELECT st_nr
+      INTO v_aktuelle_st_nr
+      FROM v_mensch_aktueller_status
+     WHERE me_nr = iME_NR;
+     
+    IF iST_NR <> v_aktuelle_st_nr THEN
+     
+        INSERT INTO v0_sl_status_logging (st_sl_nr, me_sl_nr, us_sl_nr, sl_erstellt_von, sl_kommentar )
+                                   VALUES (iST_NR  , iME_NR  , iUS_NR  , 'SYSTEM'       , iSL_KOMMENTAR);
+                                   
+        RETURN 1;
+     
+    END IF;
+
+    RETURN 0;
+END;
